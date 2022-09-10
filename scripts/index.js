df8c0087327352b686119f01d8497676a84fce81
John Carlo Ferreras <johncarloferreras@gmail.com>
2022-09-11 06:48:04 +0800

    Added search button in progress

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+// Search button in progress
+let searchBtnInProgress = document.querySelector(".search-btn");
+searchBtnInProgress.onclick = () => {
+  alert("Search function is still in progress");
+};
